d836fb04a2d0939275692941613f980ae990b3ad
Shifraz Nazar <shifraznazar@gmail.com>
2023-03-12 12:06:02 +0800

    sidebar

---
+++ src/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon from '@mui/icons-material/Search';
+import HeaderOptions from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import WorkIcon from '@mui/icons-material/Work';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header__left">
+          <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="" />
+          <div className="header__search">
+            <SearchIcon />
+            
+            <input type="text" />
+          </div>
+
+        </div>
+
+        <div className="header__right">
+            <HeaderOptions Icon={HomeIcon} title="home" />
+            <HeaderOptions Icon={SupervisorAccountIcon} title="My Network" />
+            <HeaderOptions Icon={WorkIcon} title="Jobs" />
+            <HeaderOptions Icon={ChatIcon} title="Messaging" />
+            <HeaderOptions Icon={NotificationsIcon} title="Notifications" />
+            <HeaderOptions avatar={AccountCircleIcon} title="me" />
+            
+
+
+        </div>
+
+
+
+
+    </div>
+
+  )
+}
+
+export default Header
