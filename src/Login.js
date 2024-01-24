ae4e2fc5f6537bff4ce4db6c371e9e88ac9be0a7
Shifraz Nazar <shifraznazar@gmail.com>
2024-01-25 00:29:12 +0800

    Update login

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import "./Login.css";
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
 import { login } from "./features/userSlice";
-import { ToastContainer, toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./Login.css";
 
 function Login() {
   const dispatch = useDispatch();
@@ -26,50 +26,49 @@ function Login() {
       profilePic: "https://i.pinimg.com/474x/bd/18/cb/bd18cb31dc17600e58570dbf53ce38da.jpg",
     },
     validationSchema,
-    onSubmit: (values) => {
-      auth
-        .signInWithEmailAndPassword(values.email, values.password)
-        .then((userAuth) => {
-          dispatch(
-            login({
-              email: userAuth.user.email,
-              uid: userAuth.user.uid,
-              displayName: userAuth.user.displayName,
-              profileUrl: userAuth.user.photoURL,
-            })
-          );
-          toast.success("Login successful!", { position: toast.POSITION.TOP_RIGHT });
-        })
-        .catch((error) => {
-          toast.error(error.message, { position: toast.POSITION.TOP_RIGHT });
-        });
+    onSubmit: async (values) => {
+      try {
+        const userAuth = await auth.signInWithEmailAndPassword(values.email, values.password);
+        dispatch(
+          login({
+            email: userAuth.user.email,
+            uid: userAuth.user.uid,
+            displayName: userAuth.user.displayName,
+            profileUrl: userAuth.user.photoURL,
+          })
+        );
+      } catch (error) {
+        toast.error("Login unsuccessful. Please register.");
+      }
     },
   });
 
-  const register = () => {
-    auth
-      .createUserWithEmailAndPassword(formik.values.email, formik.values.password)
-      .then((userAuth) => {
-        userAuth.user
-          .updateProfile({
-            displayName: formik.values.name,
-            photoURL: formik.values.profilePic,
-          })
-          .then(() => {
-            dispatch(
-              login({
-                email: userAuth.user.email,
-                uid: userAuth.user.uid,
-                displayName: formik.values.name,
-                photoURL: formik.values.profilePic,
-              })
-            );
-            toast.success("Registration successful!", { position: toast.POSITION.TOP_RIGHT });
-          });
-      })
-      .catch((error) => {
-        toast.error(error.message, { position: toast.POSITION.TOP_RIGHT });
+  const register = async () => {
+    try {
+      if (!formik.values.name) {
+        throw new Error("Please enter a full name");
+      }
+
+      const userAuth = await auth.createUserWithEmailAndPassword(formik.values.email, formik.values.password);
+
+      await userAuth.user.updateProfile({
+        displayName: formik.values.name,
+        photoURL: formik.values.profilePic,
       });
+
+      dispatch(
+        login({
+          email: userAuth.user.email,
+          uid: userAuth.user.uid,
+          displayName: formik.values.name,
+          profileUrl: formik.values.profilePic,
+        })
+      );
+
+      toast.success("Registration successful!");
+    } catch (error) {
+      toast.error(error.message || "Registration failed. Please try again.");
+    }
   };
 
   return (
@@ -81,52 +80,48 @@ function Login() {
 
       <form onSubmit={formik.handleSubmit}>
         <input
+          value={formik.values.name}
+          onChange={formik.handleChange}
           type="text"
           placeholder="Username"
           name="name"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.name}
-          className={formik.touched.name && formik.errors.name ? "error" : ""}
         />
-        {formik.touched.name && formik.errors.name && <div className="error">{formik.errors.name}</div>}
+        {formik.touched.name && formik.errors.name ? (
+          <div className="error">{formik.errors.name}</div>
+        ) : null}
 
         <input
+          value={formik.values.profilePic}
+          onChange={formik.handleChange}
           type="text"
           placeholder="Profile pic URL (optional)"
           name="profilePic"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.profilePic}
-          className={formik.touched.profilePic && formik.errors.profilePic ? "error" : ""}
         />
-        {formik.touched.profilePic && formik.errors.profilePic && (
+        {formik.touched.profilePic && formik.errors.profilePic ? (
           <div className="error">{formik.errors.profilePic}</div>
-        )}
+        ) : null}
 
         <input
+          value={formik.values.email}
+          onChange={formik.handleChange}
           type="email"
           placeholder="Demo@gmail.com"
           name="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-          className={formik.touched.email && formik.errors.email ? "error" : ""}
         />
-        {formik.touched.email && formik.errors.email && <div className="error">{formik.errors.email}</div>}
+        {formik.touched.email && formik.errors.email ? (
+          <div className="error">{formik.errors.email}</div>
+        ) : null}
 
         <input
+          value={formik.values.password}
+          onChange={formik.handleChange}
           type="password"
           placeholder="demo@123"
           name="password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-          className={formik.touched.password && formik.errors.password ? "error" : ""}
         />
-        {formik.touched.password && formik.errors.password && (
+        {formik.touched.password && formik.errors.password ? (
           <div className="error">{formik.errors.password}</div>
-        )}
+        ) : null}
 
         <button type="submit">Sign In</button>
       </form>
@@ -138,7 +133,7 @@ function Login() {
         </span>
       </p>
 
-      <ToastContainer />
+      <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
     </div>
   );
 }
